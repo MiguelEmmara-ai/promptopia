fcaf2c734d46484835284cec7848f637131648b3
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-09-16 00:26:36 +1200

    Refactor Nav component and add dropdown functionality

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Image from 'next/image';
 import Link from 'next/link';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
@@ -12,20 +12,18 @@ const Nav = () => {
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const fetchProviders = async () => {
-      const response = await getProviders();
-
-      setProviders(response);
-    };
-    fetchProviders();
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
   }, []);
 
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
         <Image
-          src={session?.user.image}
-          alt='Promptopia Logo'
+          src='/assets/images/logo.svg'
+          alt='logo'
           width={30}
           height={30}
           className='object-contain'
@@ -37,7 +35,7 @@ const Nav = () => {
       <div className='sm:flex hidden'>
         {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
-            <Link href='/' className='black_btn'>
+            <Link href='/create-prompt' className='black_btn'>
               Create Post
             </Link>
 
@@ -45,13 +43,13 @@ const Nav = () => {
               Sign Out
             </button>
 
-            <Link href='/profile' className='black_btn'>
+            <Link href='/profile'>
               <Image
                 src={session?.user.image}
-                alt='profile'
                 width={37}
                 height={37}
                 className='rounded-full'
+                alt='profile'
               />
             </Link>
           </div>
@@ -79,25 +77,26 @@ const Nav = () => {
         {session?.user ? (
           <div className='flex'>
             <Image
-              src='/assets/images/logo.svg'
-              alt='profile'
+              src={session?.user.image}
               width={37}
               height={37}
               className='rounded-full'
-              onClick={() => setToggleDropdown((prev) => !prev)}
+              alt='profile'
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
+
             {toggleDropdown && (
               <div className='dropdown'>
                 <Link
                   href='/profile'
-                  className='drowpdown_ink'
+                  className='dropdown_link'
                   onClick={() => setToggleDropdown(false)}
                 >
                   My Profile
                 </Link>
                 <Link
                   href='/create-prompt'
-                  className='drowpdown_ink'
+                  className='dropdown_link'
                   onClick={() => setToggleDropdown(false)}
                 >
                   Create Prompt
