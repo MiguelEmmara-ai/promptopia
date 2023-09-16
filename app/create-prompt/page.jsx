535c9654a9d991d2392ddec08ebcef8d19b4fa73
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-09-16 18:12:40 +1200

    Add createPrompt function to handle form submission

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,25 +7,45 @@ import { useState } from 'react';
 import Form from '@components/Form';
 
 const CreatePrompt = () => {
-  const [submitting, setSubmitting] = useState(false)
+  const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: '',
-    tags: ''
-  })
+    tags: '',
+  });
 
   const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
 
-  }
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        Router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
-    <Form 
-    type="Create"
-    post={post}
-    setPost={setPost}
-    submitting={submitting}
-    handleSubmit={createPrompt}
+    <Form
+      type='Create'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
     />
-  )
+  );
 };
 
 export default CreatePrompt;
